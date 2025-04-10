8b8aad8c8a7e7354bad00e06623340598659dc16
Gabriel <lopes.biel2588@gmail.com>
2025-04-10 00:04:52 -0300

    first commit

---
+++ Eai/src/test/java/com/eai/Eai/EaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eai.Eai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
